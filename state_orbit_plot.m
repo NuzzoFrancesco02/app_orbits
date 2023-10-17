eaba043158fc626f0d8e5dce476390d083f55919
NuzzoFrancesco02 <francesco1.nuzzo@mail.polimi.it>
2023-10-17 22:58:51 +0200

    Stop and play button

--- state_orbit_plot.m
+++ state_orbit_plot.m
@@ -4,7 +4,7 @@ v = [45 30 30];
 view(ax,v);
 plot3(ax,r(1,end),r(2,end),r(3,end),'or','MarkerFaceColor',"#77AC30",'MarkerEdgeColor',"#77AC30",'MarkerSize',10);
 hold(ax,"on");
-plot3(ax,r(1,1:end),r(2,1:end),r(3,1:end),'Color',"#A2142F");
+plot3(ax,r(1,1:end),r(2,1:end),r(3,1:end),'Color',"#A2142F",'LineStyle','-');
 grid(ax,"on");
 Rt=6378;
 Terra3d(ax,Rt);
