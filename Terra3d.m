9256abb0882de671d71c6c5d90ea9682e0ecae90
NuzzoFrancesco02 <francesco1.nuzzo@mail.polimi.it>
2023-10-17 22:34:11 +0200

    solved static plot

--- Terra3d.m
+++ Terra3d.m
@@ -1,22 +1,22 @@
-function Terra3d(Rt)
+function Terra3d(ax,Rt)
 % plotta la terra in 3d con il suo raggio nell origine degli assi
 % basta richiamare la funzione
-
+hold(ax,"on")
 if nargin==0
     Rt=6378;
 end
 
 load('topo.mat','topo','topomap1');
 % whos topo topomap1
-contour(0:359,-89:90,topo,[0 0],'b')
-axis equal
+contour(ax,0:359,-89:90,topo,[0 0],'b')
+axis(ax,"equal")
 % box on
 % set(gca,'XLim',[0 360],'YLim',[-90 90], ...
 %     'XTick',[0 60 120 180 240 300 360], ...
 %     'Ytick',[-90 -60 -30 0 30 60 90]);
-hold on
-image([0 360],[-90 90],topo,'CDataMapping', 'scaled');
-colormap(topomap1)
+hold(ax,"on")
+image(ax,[0 360],[-90 90],topo,'CDataMapping', 'scaled');
+colormap(ax,topomap1)
 [x,y,z] = sphere(100);
 % cla reset
 % axis square off
@@ -29,8 +29,8 @@ props.FaceColor= 'texture';
 props.EdgeColor = 'none';
 props.FaceLighting = 'phong';
 props.Cdata = topo;
-surface(x*Rt,y*Rt,z*Rt,props);
-light('position',[1 1 1]);
-light('position',[-1.5 0.5 -0.5], 'color', [.6 .2 .2]);
+surface(ax,x*Rt,y*Rt,z*Rt,props);
+light(ax,'position',[1 1 1]);
+light(ax,'position',[-1.5 0.5 -0.5], 'color', [.6 .2 .2]);
 
-% quiver3(0,0,0,0,0,5*Rt,'--k')
+quiver3(ax,0,0,0,0,0,5*Rt,'--k')
